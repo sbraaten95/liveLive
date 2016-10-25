f538a3c1b6ee7cc37f9fcce87976eb93795d68ef
Scott Braaten <sbraaten95@gmail.com>
2016-10-25 10:39:05 -0400

    another update

--- client/assets/homeControl.js
+++ client/assets/homeControl.js
@@ -1,3 +1,9 @@
-app.controller('homeControl', ['$location', function ($location){
+app.controller('homeControl', ['$location', '$cookies', 'userFactory', function ($location, $cookies, uF){
 	var _this = this;
+	this.login=()=>{
+		uF.login(_this.reqUser, (user)=>{
+			$cookies.put('user', user._id);
+			$location.path('/dashboard');
+		});
+	};
 }]);
